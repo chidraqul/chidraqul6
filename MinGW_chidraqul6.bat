297a16c026cfa440a97978a7719af966de86ccbb
Chiller Dragun <ChillerDragon@gmail.com>
2017-12-03 12:49:18 -0800

    more MinGW compiler helper imrovmements

--- MinGW_chidraqul6.bat
+++ MinGW_chidraqul6.bat
@@ -1,5 +1,6 @@
 @echo off
 title chidraqul6 g++ compiler (boost) [ChillerDragon]
+if not exist "boost_path.txt" echo add/path/to/boost/lib/here > boost_path.txt
 set /p boost_path=<boost_path.txt
 if not exist "objs\" mkdir objs\
 :compile
@@ -11,8 +12,7 @@ gcc src/*.c -c
 ::g++ src/*.cpp -c
 g++ -I %boost_path% src/*.cpp -c
 g++ *.o -o chidraqul6
-echo ========= clean up =========
-move *.o objs
+move *.o objs >NUL
 echo ============================
 echo finished compile. press any key to compile agian
 pause >NUL
