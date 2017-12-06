f139297733e095b5debe29ae7dad73bb3801a6f7
ChillerDragon <ChillerDragon@gmail.com>
2017-12-06 15:22:19 +0100

    no oop pls

--- src/client/chidraqul6.cpp
+++ src/client/chidraqul6.cpp
@@ -6,7 +6,6 @@
 #include "world.h"
 #include "controls.h"
 #include "render.h"
-#include "game.h"
 
 #ifdef _WIN32
 #include "networking_client_win.h"
@@ -14,14 +13,14 @@
 #include "networking_client_osx.h"
 #endif
 
-CGame *pGame = new CGame;
+CPlayer *pPlayer = new CPlayer;
 
 void OnTick()
 { 
-    HandleInputs(*pGame->pPlayer); //creates LastInpDirX
-	pGame->pPlayer->OnTick(); //uses LastInpDirX to keep moving in fall
-	int recv_pos = SendPosition(pGame->pPlayer->PosX);
-	RenderFrame(*pGame->pPlayer, recv_pos);
+    HandleInputs(*pPlayer); //creates LastInpDirX
+	pPlayer->OnTick(); //uses LastInpDirX to keep moving in fall
+	int recv_pos = SendPosition(pPlayer->PosX);
+	RenderFrame(*pPlayer, recv_pos);
 	//player_net.PosX = recv_pos;
 #ifdef __APPLE__
     //system("sleep 0.000000001"); //shit xd
@@ -43,13 +42,8 @@ int main()
 	initscr();
     system("stty -icanon time 0 min 0");
 #endif // __APPLE__
-    
-	pGame->InitPlayers();
-	
-	for (int i = 0; i < MAX_PLAYERS; i++)
-	{
-		pGame->apPlayers[i]->Spawn();
-	}
+
+	pPlayer->Spawn();
 
     while (true)
     {
