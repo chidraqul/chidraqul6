9a13bbe97886273ecfddb8e3d5f0e343ab4b0e0d
Chiller Dragon <chillerdragon@gmail.com>
2018-02-23 01:47:31 +0100

    xcode > visual studio tabbing

--- src/server/server.h
+++ src/server/server.h
@@ -8,27 +8,26 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+    typedef struct
+    {
+        int ClientID;
+        int NetState; //idk yet what this is probably some check if this player exsist or something
+        
+        int PosX;
+        int PosY;
+        int IsAlive;
+    } Player;
     
-	typedef struct
-	{
-		int ClientID;
-		int NetState; //idk yet what this is probably some check if this player exsist or something
-
-		int PosX;
-		int PosY;
-		int IsAlive;
-	} Player;
-
-	typedef struct
-	{
-		int Port;
-		char aServername[32];
-	} ServerSettings;
-
-
-
-
-
+    typedef struct
+    {
+        int Port;
+        char aServername[32];
+    } ServerSettings;
+    
+    
+    
+    
     /*
      Function: GetPlayerPosX
      Feteches the player position out of client buffers.
@@ -42,19 +41,19 @@ extern "C" {
     
     int GetPlayerPosX(const char * pData);
     
-	/*
-	Function: GetPlayerPosY
-	Feteches the player position out of client buffers.
-
-	Parameters:
-	buffer - Pointer to the client data buffer.
-
-	Returns:
-	The Y position of the player as an integer
-	*/
-
-	int GetPlayerPosY(const char * pData);
-
+    /*
+     Function: GetPlayerPosY
+     Feteches the player position out of client buffers.
+     
+     Parameters:
+     buffer - Pointer to the client data buffer.
+     
+     Returns:
+     The Y position of the player as an integer
+     */
+    
+    int GetPlayerPosY(const char * pData);
+    
     /*
      Function: GetPlayerID
      Feteches the player ClientID out of client buffers.
@@ -83,7 +82,7 @@ extern "C" {
      */
     
     void MainDataJuggeling(char * pClientData);
-
+    
     /*
      Function: GetNextClientID
      Checks for the next (lowest) unused client id
@@ -93,26 +92,27 @@ extern "C" {
      -1 on failure (server full)
      */
     
-	int GetNextClientID();
-
+    int GetNextClientID();
+    
     /*
      Function: InitServer
      For now just cleaning the players array
      */
     
-	void InitServer(ServerSettings * pSettings);
+    void InitServer(ServerSettings * pSettings);
+    
+    /*
+     Function: LoadServerSettings
+     Reads server.cfg file in the chidraqul6 directory
+     and stores results in the ServerSettings struct
+     
+     Returns:
+     -1 on failure
+     0 on success
+     */
+    
+    int LoadServerSettings(ServerSettings * pSettings);
 
-	/*
-	Function: LoadServerSettings
-	Reads server.cfg file in the chidraqul6 directory
-	and stores results in the ServerSettings struct
-
-	Returns:
-	-1 on failure
-	0 on success
-	*/
-
-	int LoadServerSettings(ServerSettings * pSettings);
 
 #ifdef __cplusplus
 }
